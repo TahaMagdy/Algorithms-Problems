af0b7c2c337e80403ede313b1596b2e3a5232fba
TahaMagdy <t.tahamagdyt@outlook.com>
2017-05-17 00:25:35 +0200

    add :"D traveledDistance

--- rocket.py
+++ rocket.py
@@ -5,23 +5,23 @@
 # t is the Duration -- the time of rocket working
 class Rocket:
 
-
     ##
     def __init__(self, n, a, t):
         self.n = n
         self.a = a
         self.t = t
-        init_velocity = 0
+        initVelocity = 0
+        traveledDistance = 0
 
     ##
     # Calculate the distance that is cut with this rocket
     def traveled_distance(self, rocket):
-        return (self.init_velocity * self.t) + (0.5 * self.a * self.t * self.t)
+        return (self.initVelocity * self.t) + (0.5 * self.a * self.t * self.t)
 
     ##
     # Calculate the final velocity of the ship after launching this rocket
     def final_velocity(self):
-        return self.init_velocity + (self.a * self.t)
+        return self.initVelocity + (self.a * self.t)
 ###
 #END CLASS Rocket
 ###
@@ -30,6 +30,6 @@ class Rocket:
 
 #TESTING
 obj = Rocket(2,3,4)
-obj.init_velocity = 0
+obj.initVelocity = 0
 print obj.traveled_distance( obj)
 print obj.final_velocity()
